fa4b4f346204525cb2fdd533a972bc655597b4cb
patanjalisharma <patanjali.sharma03@gmail.com>
2025-09-04 16:06:27 +0530

    clerk added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
-
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/theme-provider"
@@ -23,6 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider>
     <html lang="en" className={`${poppins.variable}`} suppressHydrationWarning>
       <body
             >
@@ -38,5 +41,6 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,13 @@ import Link from "next/link";
 import NavItems from "./NavItems";
 import {  Bug, Menu, X } from "lucide-react";
 import { useState } from "react";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
 
 const Navbar = () => {
@@ -20,6 +27,16 @@ const Navbar = () => {
         {/* Desktop Nav */}
         <div className="hidden sm:flex items-center gap-8">
           <NavItems />
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign In
+                </button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
           
         </div>
 
@@ -35,6 +52,17 @@ const Navbar = () => {
       {isOpen && (
         <div className="sm:hidden mt-4 flex flex-col gap-4">
           <NavItems />
+           <SignedOut>
+            <SignInButton mode="modal">
+              <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign In
+                </button>
+            </SignInButton>
+            
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
           
         </div>
       )}
